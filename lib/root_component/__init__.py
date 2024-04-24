f8678e36b7a0624b458f04514256deabd921b6ad
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-04-24 14:27:23 +0800

    fix: change the _RELEASE to True for deployment

--- lib/root_component/__init__.py
+++ lib/root_component/__init__.py
@@ -2,20 +2,22 @@ import os
 
 import streamlit.components.v1 as components
 
-_RELEASE = False
 
+_RELEASE = True
 if not _RELEASE:
     _component_func = components.declare_component(
         "root_component",
         url="http://localhost:5300",
     )
+    print("Using development build")
 else:
     parent_dir = os.path.dirname(os.path.abspath(__file__))
-    build_dir = os.path.join(parent_dir, "frontend/dist/browser")
+    build_dir = os.path.join(parent_dir, "frontend/dist/frontend/browser")
     _component_func = components.declare_component(
         "root_component",
         path=build_dir
     )
+    print("Using production build")
 
 def root_component(data, key=None) -> dict[str, str]:
     component_value = _component_func(data=data, key=key, default=0)
