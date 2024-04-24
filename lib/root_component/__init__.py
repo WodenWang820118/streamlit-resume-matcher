0624fe2763738e37653bf7cbf09cf8a13516d87c
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-04-24 16:14:30 +0800

    fix: use url instead of build as component

--- lib/root_component/__init__.py
+++ lib/root_component/__init__.py
@@ -9,15 +9,12 @@ if not _RELEASE:
         "root_component",
         url="http://localhost:5300",
     )
-    print("Using development build")
 else:
     parent_dir = os.path.dirname(os.path.abspath(__file__))
-    # build_dir = os.path.join(parent_dir, "frontend", "dist", "frontend" ,"browser")
     _component_func = components.declare_component(
         "root_component",
-        url="https://resume-matcher.netlify.app",
+        url="https://resume-matcher.netlify.app/",
     )
-    print("Using production build")
 
 def root_component(data, key=None) -> dict[str, str]:
     component_value = _component_func(data=data, key=key, default=0)
